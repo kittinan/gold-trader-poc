f32bb541d71eaf0855a16a68eb2d0e68fb76f4d5
Kittinan <tunxedge@gmail.com>
2026-02-05 12:31:08 +0700

    fix: set DJANGO_SETTINGS_MODULE and SECRET_KEY in CI test job
    - Update settings/__init__.py to default to dev settings
    - Add environment variables to pytest job in CI

--- backend/gold_trader/settings/__init__.py
+++ backend/gold_trader/settings/__init__.py
@@ -1,4 +1,12 @@
 # Settings package for Gold Trader
-# Use: DJANGO_SETTINGS_MODULE=gold_trader.settings.dev python manage.py runserver
+# Default to dev settings if DJANGO_SETTINGS_MODULE is not explicitly set
+# Use: DJANGO_SETTINGS_MODULE=gold_trader.settings.prod python manage.py runserver
 
-from .base import *  # noqa: F401, F403
+import os
+
+# If this file is imported directly (as gold_trader.settings), use dev by default
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'gold_trader.settings':
+    from .dev import *  # noqa: F401, F403
+else:
+    # Allow importing base for extension
+    pass
